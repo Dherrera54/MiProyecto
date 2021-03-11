08e229b806ab08eb10b047fa42b24a25e4c40c45
Dario_Herrera <d.herrerag@uniandes.edu.co>
2021-03-10 21:40:40 -0500

    prueba7

--- src/logica/coleccion.py
+++ src/logica/coleccion.py
@@ -5,7 +5,7 @@ from src.modelo.declarative_base import engine, Base, session
 
 
 class Coleccion():
-  ### prueba cambio 6
+  ### prueba cambio 7
     def __init__(self):
         Base.metadata.create_all(engine)
 
